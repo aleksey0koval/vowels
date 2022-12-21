7ea4c891ec788f90d44d34c51339a16dd516cb89
Aleksey Kovalchuk <aleksey0koval@gmail.com>
2022-12-21 12:55:22 +0100

    build: init spring boot project

---
+++ src/main/java/com/kavalchuk/vowels/VowelsApplication.java
@@ -0,0 +1,11 @@
+package com.kavalchuk.vowels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VowelsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VowelsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kavalchuk/vowels/VowelsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kavalchuk.vowels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VowelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
